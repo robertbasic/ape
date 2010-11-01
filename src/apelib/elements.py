dc46aeef41412d015ccaff4e010d9eced299e4c3
Robert Basic <robertbasic.com@gmail.com>
2010-11-01 22:58:05 +0100

    the file browser widget now shows only the HOME directory

--- src/apelib/elements.py
+++ src/apelib/elements.py
@@ -1,6 +1,7 @@
 __author__="robert"
 __date__ ="$Oct 31, 2010 2:45:50 PM$"
 
+import os
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
@@ -13,10 +14,22 @@ class apeFileBrowser(QWidget):
     def __init__(self, parent):
         QWidget.__init__(self)
 
+        homePath = os.path.expanduser('~')
+
         model = QFileSystemModel()
-        model.setRootPath(QDir.currentPath())
+        model.setRootPath(homePath)
+
+        index = model.index(homePath)
+
         tree = QTreeView(self)
+        tree.setHeaderHidden(True)
+
         tree.setModel(model)
+        tree.setRootIndex(index)
+
+        tree.hideColumn(1)
+        tree.hideColumn(2)
+        tree.hideColumn(3)
 
         parent.setWidget(tree)
 
