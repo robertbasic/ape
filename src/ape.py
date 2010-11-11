213308dee52dc2652dc0d09e21f9c5d340204d38
Robert Basic <robertbasic.com@gmail.com>
2010-11-11 22:28:23 +0100

    gah! of course I put the check for opened documents in the wrong place. stupid.

--- src/ape.py
+++ src/ape.py
@@ -35,19 +35,18 @@ class apeMain(QMainWindow):
         self.fileBrowser = elements.apeFileBrowser(self.gui.filesDock, self)
 
     def addNewDocument(self, path):
-        document = elements.apeDocument(path)
-        if(document.valid):
-            alreadyOpen = self.documents.isDocumentAlreadyOpen(path)
-
-            # 0 == False gives True!
-            if(alreadyOpen is False):
+        alreadyOpen = self.documents.isDocumentAlreadyOpen(path)
+        # 0 == False gives True!
+        if(alreadyOpen is False):
+            document = elements.apeDocument(path)
+            if(document.valid):
                 index = self.documents.tabs.addTab(document, document.fileName)
 
                 self.documents.open[unicode(path.toUtf8(), 'utf-8')] = index
 
                 self.documents.tabs.setCurrentIndex(index)
-            else:
-                self.documents.tabs.setCurrentIndex(alreadyOpen)
+        else:
+            self.documents.tabs.setCurrentIndex(alreadyOpen)
 
     def closeDocument(self, index):
         return False
